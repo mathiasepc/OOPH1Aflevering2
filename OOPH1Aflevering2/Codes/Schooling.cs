e500beede744db7ef60ef474b5511d7dc0b0472a
mathias clausen <mathiasepc@msn.com>
2022-02-28 18:48:10 +0100

    Ændrede på noget så jeg kan følge opgaven

--- OOPH1Aflevering2/Codes/Schooling.cs
+++ OOPH1Aflevering2/Codes/Schooling.cs
@@ -13,19 +13,18 @@ namespace OOPH1Aflevering2.Codes
         public List<TECPerson> Teachers { get; set; }
         public List<string> Courses { get; set; }
 
-        public Schooling(List<string> courses)
-        {
-            Courses = courses;
-        }
-
         public virtual void SetCourses(List<string>courses)
         {
+            List<string> course= new();
+            Courses = courses;
             foreach (string displayCourses in (Enum.GetNames(typeof(CourseCategory))))
             {
                 Console.WriteLine(displayCourses.ToString());
                 courses.Add(displayCourses);
             }
         }
+
+       
     }
 }
 
