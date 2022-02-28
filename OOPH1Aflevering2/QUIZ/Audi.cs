50edaf8be6630285ca28224ac370d9dd2a8bff2a
mathias clausen <mathiasepc@msn.com>
2022-02-28 10:11:44 +0100

    Add project files.

---
+++ OOPH1Aflevering2/QUIZ/Audi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH1Aflevering2.QUIZ
+{
+    internal class Audi : Motorkøretøj
+    {
+        public string Model { get; set; }
+    }
+}
---
+++ OOPH1Aflevering2/QUIZ/Bil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPH1Aflevering2.QUIZ
+{
+    internal class Bil : Motorkøretøj
+    {
+        public string Mærke { get; set; }
+
+        public string Model { get; set; }
+
+        public int HesteKræfter { get; set; }
+    }
+}
