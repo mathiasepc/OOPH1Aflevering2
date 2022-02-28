c33e03dd977223d0c1d2ff685c47031152e5d1d1
mathias clausen <mathiasepc@msn.com>
2022-02-28 18:37:52 +0100

    init, mit første push

--- OOPH1Aflevering2/Codes/Schooling.cs
+++ OOPH1Aflevering2/Codes/Schooling.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace OOPH1Aflevering2.Codes
 {
-    
+
     internal class Schooling
     {
         public SchoolingCategory SchoolingName { get; set; }
         public List<TECPerson> Teachers { get; set; }
         public List<string> Courses { get; set; }
+
+        public Schooling(List<string> courses)
+        {
+            Courses = courses;
+        }
+
+        public virtual void SetCourses(List<string >courses)
+        {
+            foreach (string displayCourses in (Enum.GetNames(typeof(CourseCategory))))
+            {
+                Console.WriteLine(displayCourses.ToString());
+                courses.Add(displayCourses);
+            }
+        }
     }
 }
+
--- OOPH1Aflevering2/Program.cs
+++ OOPH1Aflevering2/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿List<string> p = new();
+
+SchoolingCategory s = new();
+
+Schooling a = new(p);
+
+a.SetCourses(p);
